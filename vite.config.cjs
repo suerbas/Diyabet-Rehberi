d886076e406a0e01e17f5639b88ec0b618bbf4db
suerbas <suerbas@gmail.com>
2025-11-21 16:35:22 +0300

    Convert vite.config.cjs to ES module syntax

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,16 +1,17 @@
-const path = require('path');
-const { defineConfig } = require('vite');
-const react = require('@vitejs/plugin-react');
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-module.exports = defineConfig({
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
+export default defineConfig({
+    base: './',
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
     },
-  },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      }
+    }
 });

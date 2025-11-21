f96bbd6ef5d317f4ec79df4dd9b39a3c3b2b3d1e
suerbas <suerbas@gmail.com>
2025-11-21 16:02:47 +0300

    Convert vite.config.ts to CommonJS format

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react');
+
+module.exports = defineConfig({
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
+});
